e73550c6c3386342b28e807486a024bb4f3ffd3d
hpetrow <heather.petrow@gmail.com>
2015-07-24 10:26:00 -0400

    done

--- js/calc.js
+++ js/calc.js
@@ -6,3 +6,25 @@ $(function(){
 });
 
 // your code here!
+
+function Calculator() {
+  this.equals();
+}
+
+Calculator.prototype.equals = function() {
+  $("#equals").click(function() {
+    var num1 = parseInt($("#number1").val());
+    var num2 = parseInt($("#number2").val());
+    var oper = $("#operation").val();
+
+    if (isNaN(num1) || isNaN(num2)) {
+      $("#result").text("Sorry, one of those is not a valid number!");
+    }
+    else if(oper.match(/[+-\/\*]/gi) === null || oper.match(/[+-\/\*]/gi).length > 1) {
+      $("#result").text("Sorry, not a valid operation!");
+    }
+    else {
+      $("#result").text(eval(num1 + oper + num2));
+    }
+  });
+}
